89387f64e94330b614b7429569d8144f51fd7cd3
Damian Sire <damiansire@hotmail.com>
2021-09-10 23:20:33 -0300

    Feat: Add RT and Fav logic and blocklist

--- app.js
+++ app.js
@@ -1,29 +1,47 @@
 const { twitterKeys } = require("./notocar/keys")
 
-// Paquete Twit
-const Twit = require('twit');
+const blocklist = ["damiansire"]
 
-const T = new Twit({
-    consumer_key: "TU CONSUMER KEY", // Se Generó cuando creamos la API en Twitter Developers
-    consumer_secret: "TU CONSUMER SECRET", // Se Generó cuando creamos la API en Twitter Developers
-    access_token: "TU ACCESS TOKEN", // Se Generó cuando creamos la API en Twitter Developers
-    access_token_secret: "TU ACCESS TOKEN SECRET", // Se Generó cuando creamos la API en Twitter Developers
-    timeout_ms: 60 * 20000, // Ejecutar tarea cada 20 segundos
-    strictSSL: true, // Le indico que trabaje los procesos con SSL (Secure Sockets Layer o Capa de Sockets Seguros)
-});
+try {
+    // Paquete Twit
+    const Twit = require('twit');
+    const T = new Twit(twitterKeys);
 
-// Retwittear Tweets que contenga el Hashtag #css
-const stream = T.stream('statuses/filter', { track: '#css' });
+    // Retwittear Tweets que contenga el Hashtag #css
+    const stream = T.stream('statuses/filter', { track: '#damiansire10k' });
+    console.log("Iniciado :D")
 
-// Registro errores en las solicitudes
-function responseCallback(err, data, response) {
-    console.log(err);
+    // Registro errores en las solicitudes
+    function responseCallback(err, data, response) {
+        if (err) {
+            console.log(err.message);
+            console.log(err.allErrors);
+            return;
+        }
+        console.log("Tamos ok bb")
+    }
+
+
+
+    // Tareas
+    stream.on('tweet', tweet => {
+        console.log("Blocklist:")
+        console.log(blocklist)
+        if (!blocklist.includes(tweet.user.screen_name)) {
+            // Retweet
+            T.post('statuses/retweet/:id', { id: tweet.id_str }, responseCallback);
+            // Me Gusta
+            T.post('favorites/create', { id: tweet.id_str }, responseCallback);
+            console.log(tweet.text);
+            console.log(tweet.user.name);
+            console.log(tweet.user.screen_name);
+            console.log(tweet.user.location);
+        }
+        else {
+            console.log(`Bloqueado: ${tweet.user.screen_name} ${tweet.text}`)
+        }
+    });
+
+} catch (error) {
+    console.log("Exploto :)")
 }
-
-// Tareas
-stream.on('tweet', tweet => {
-    // Retweet
-    T.post('statuses/retweet/:id', { id: tweet.id_str }, responseCallback);
-    // Me Gusta
-    T.post('favorites/create', { id: tweet.id_str }, responseCallback);
-});
