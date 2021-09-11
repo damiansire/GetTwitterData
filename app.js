8a0fa136f056e8099672c23903b2e94a72446d26
Damian Sire <damiansire@hotmail.com>
2021-09-11 00:28:56 -0300

    Base de la logica

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
+const Twit = require('twit');
 const fs = require("fs");
+const { Client } = require('pg');
+
+const { dbKeys } = require('./notocar/dbconfig')
 const { twitterKeys } = require("./notocar/keys")
+
+const client = new Client(dbKeys);
+
+//const query = "CREATE TABLE Tweets (usuario varchar(255), tweet varchar(255));";
+
+const execute = (query) => {
+    client.connect();
+    client.query(query, (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) {
+            console.log(JSON.stringify(row));
+        }
+        client.end();
+    });
+}
+
+const insertTweet = (client, user, message) => {
+    const query = "INSERT INTO Tweets (usuario, tweet) VALUES ('damiansire', 'este es un mensaje de prueba');";
+    execute(query, client);
+}
+
+const selectTweets = (client) => {
+    const query = "SELECT * FROM Tweets;";
+    execute(query, client);
+}
+
+selectTweets(client);
+
+//insertTweet(client);
 
 const blocklistNamesObj = JSON.parse(fs.readFileSync("blocklist.json"));
 const blocklist = blocklistNamesObj.map(nameObs => nameObs.name);
 
 try {
     // Paquete Twit
-    const Twit = require('twit');
+
     const T = new Twit(twitterKeys);
 
     // Retwittear Tweets que contenga el Hashtag #css
@@ -18,9 +51,10 @@ try {
         if (err) {
             console.log(err.message);
             console.log(err.allErrors);
-            return;
         }
-        console.log("Tamos ok bb")
+        else {
+            console.log("Estamos ok :D")
+        }
     }
 
 
@@ -38,6 +72,8 @@ try {
             console.log(tweet.user.name);
             console.log(tweet.user.screen_name);
             console.log(tweet.user.location);
+
+            //T.post('statuses/update', { status: `Gracias por tu tweet @${tweet.user.screen_name} lo aprecio mucho!!!` }, responseCallback)
         }
         else {
             console.log(`Bloqueado: ${tweet.user.screen_name} ${tweet.text}`)
